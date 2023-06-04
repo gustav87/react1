0de7fd9ac483946841fea279ade943c26bdbae1f
Gustav Andersson <gustav87and@gmail.com>
2023-06-04 17:09:43 +0200

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { Outlet } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
-import AcceptButton from '@/components/AcceptButton';
-import RejectButton from '@/components/RejectButton';
 import NavBar from '@/components/NavBar';
 
 function App() {
   return <>
     <div className="App">
-      <header className="App-header">
-        <NavBar></NavBar>
+      <NavBar></NavBar>
+      <div className="main">
         <Outlet />
-        <AcceptButton></AcceptButton>
-        <RejectButton></RejectButton>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+      </div>
     </div>
   </>
 }
 
 export default App;
+
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,30 @@
+import './NavBar.css';
+
 function NavBar() {
+  const navItemStyle = "hover:text-blue-800 uppercase font-bold text-white hover:text-white hover:bg-sky-700";
   return (
-    <ul class="flex">
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="/">About</a>
-      </li>
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="/hi">Hi</a>
-      </li>
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="/projects">Projects</a>
-      </li>
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="/contact-me">Contact Me</a>
-      </li>
-      <li class="mr-6">
-        <a class="text-gray-400 cursor-not-allowed" href="#">Disabled</a>
-      </li>
-    </ul>
+    <div className="flex p-6 bg-zinc-700 align-middle">
+      <ul className="flex">
+        <li className="mr-12">
+          <a className={navItemStyle} href="/">
+            <img src="/klaus.png" width="50px" className="klaus"/>
+          </a>
+        </li>
+        <li className="mr-6">
+          <a className={navItemStyle} href="/projects">Projects</a>
+        </li>
+        <li className="mr-6">
+          <a className={navItemStyle} href="/contact">Contact</a>
+        </li>
+        <li className="mr-6">
+          <a className={navItemStyle} href="/hi">Hi</a>
+        </li>
+        <li className="mr-6">
+          <a className="text-gray-400 cursor-not-allowed uppercase font-bold" href="#">Disabled</a>
+        </li>
+      </ul>
+    </div>
   );
 }
 
 export default NavBar;
-
