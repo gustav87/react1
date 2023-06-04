271ed0d33680967b93325a78c05762ce53824f8a
Gustav Andersson <gustav87and@gmail.com>
2023-06-05 01:28:13 +0200

    Update Frontpage text

--- src/components/Frontpage.tsx
+++ src/components/Frontpage.tsx
@@ -1,9 +1,10 @@
 function Frontpage() {
   return <>
     <div className="text-left max-w-5xl">
-      <p className="mb-8">My name is Gustav Andersson and I'm a web developer from Sweden.</p>
-      <p className="mb-8">This page is made with React, Vite and Tailwind CSS in the frontend, C# .NET in the backend and a MongoDB database.</p>
-      <p className="mb-8">The code is open source on GitHub, and the app is automatically built and packaged into a Docker image on Docker Hub whenever new code is merged.</p>
+      <p className="mb-7">My name is Gustav Andersson and I'm a web developer from Sweden.</p>
+      <p className="mb-7">This page is made with React, Vite and Tailwind CSS in the frontend, C# .NET in the backend and a MongoDB database.</p>
+      <p className="mb-7">The code is open source on GitHub.</p>
+      <p className="mb-7">There is a CI/CD pipeline on Azure which automatically builds the app and deploys it as a Docker container which serves this website.</p>
       <p>The project serves as a learning tool and playground for me.</p>
     </div>
   </>
