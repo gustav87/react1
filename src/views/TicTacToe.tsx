5dff665e70ee8bde15dd5c4ef3eb60b4bac49c1c
Gustav Andersson <gustav87and@gmail.com>
2023-06-05 18:15:13 +0200

    dummy change

--- src/views/TicTacToe.tsx
+++ src/views/TicTacToe.tsx
@@ -2,7 +2,7 @@ const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 function TicTacToe() {
   return <>
-    <h1 className="text-2xl mb-5" onClick={handleClick}>Here's a Tic Tac Toe game I made.</h1>
+    <h1 className="text-2xl mb-5" onClick={handleClick}>Here's a Tic Tac Toe game I made!</h1>
     <div onClick={send}>Test backend</div>
   </>
 }
