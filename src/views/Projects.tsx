1b4eb5b45b73b18f4110a4b6464c3bfb428374ff
Gustav Andersson <gustav87and@gmail.com>
2023-06-06 16:32:40 +0200

    fix projects styling

--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -3,8 +3,10 @@ import { Outlet } from "react-router-dom";
 function Projects() {
   return <>
     <h1 className="text-4xl mb-10">Projects!</h1>
-    <a href="/projects/tictactoe" className="hover:text-blue-800">Tic Tac Toe</a>
-    <a href="/projects/state" className="hover:text-blue-800">State</a>
+    <div className="flex justify-around w-1/4 mb-10">
+      <a href="/projects/tictactoe" className="hover:text-blue-800">Tic Tac Toe</a>
+      <a href="/projects/state" className="hover:text-blue-800">State</a>
+    </div>
     <Outlet/>
   </>
 }
