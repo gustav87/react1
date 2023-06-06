03aeb1d77a2ca9683d2ab04679e644639e4544b6
Gustav Andersson <gustav87and@gmail.com>
2023-06-06 16:46:14 +0200

    state project styling

--- src/views/State.tsx
+++ src/views/State.tsx
@@ -8,18 +8,23 @@ function State() {
   let sculpture = sculptureList[index];
 
   return <>
-    <button className="text-red-500 hover:text-red-300" onClick={() => setColor("red")}>Red state</button>
-    <button className="text-green-500 hover:text-green-300" onClick={() => setColor("green")}>Green state</button>
-    <div className={`mt-5 w-20 bg-transparent text-white text-center cursor-default border-2 bg-${colorState}-400`} onClick={getColor}>State</div>
-
-    <button onClick={incrementIndex}>Next</button>
-    <h2>
-      <i>{sculpture.name} </i>
-      by {sculpture.artist}
-    </h2>
-    <h3>({index + 1} of {sculptureList.length})</h3>
-    <img src={sculpture.url} alt={sculpture.alt}/>
-    <p>{sculpture.description}</p>
+    <div className="flex justify-around w-2/4">
+      <div className="w-2/4">
+        <button className="text-red-500 hover:text-red-300 block" onClick={() => setColor("red")}>Red state</button>
+        <button className="text-green-500 hover:text-green-300 block" onClick={() => setColor("green")}>Green state</button>
+        <div className={`mt-5 w-20 bg-transparent text-white text-center cursor-default border-2 bg-${colorState}-400`} onClick={getColor}>State</div>
+      </div>
+      <div className="w-2/4">
+        <button className="border-2 px-2 hover:text-black" onClick={incrementIndex}>Next</button>
+        <h2>
+          <i>{sculpture.name} </i>
+          by {sculpture.artist}
+        </h2>
+        <h3>({index + 1} of {sculptureList.length})</h3>
+        <img src={sculpture.url} alt={sculpture.alt}/>
+        <p>{sculpture.description}</p>
+      </div>
+    </div>
   </>
 
   function incrementIndex() {
