92e51f2d5d137c079d5186ce1997a96705151c91
Gustav Andersson <gustav87and@gmail.com>
2023-06-06 16:26:12 +0200

    Implement state stuff

--- src/views/State.tsx
+++ src/views/State.tsx
@@ -1,16 +1,43 @@
 const backend_url = import.meta.env.VITE_BACKEND_URL;
+import { sculptureList } from '@/data/stateData.js';
+import { useState } from 'react';
 
 function State() {
-  return <>
-    <h1 className="text-2xl mb-5">State stuff</h1>
-    <button className="text-red-500 hover:text-red-300" onClick={() => setState("red")}>Red state</button>
-    <button className="text-green-500 hover:text-green-300" onClick={() => setState("green")}>Green state</button>
-    <div className="mt-5 w-20 bg-transparent text-white text-center cursor-default border-2" onClick={setState}>State</div>
-  </>
-}
+  const [index, setIndex] = useState(0);
+  const [colorState, setColorState] = useState("transparent");
+  let sculpture = sculptureList[index];
 
-function setState(color) {
-  console.log(color);
+  return <>
+    <button className="text-red-500 hover:text-red-300" onClick={() => setColor("red")}>Red state</button>
+    <button className="text-green-500 hover:text-green-300" onClick={() => setColor("green")}>Green state</button>
+    <div className={`mt-5 w-20 bg-transparent text-white text-center cursor-default border-2 bg-${colorState}-400`} onClick={getColor}>State</div>
+
+    <button onClick={incrementIndex}>Next</button>
+    <h2>
+      <i>{sculpture.name} </i>
+      by {sculpture.artist}
+    </h2>
+    <h3>({index + 1} of {sculptureList.length})</h3>
+    <img src={sculpture.url} alt={sculpture.alt}/>
+    <p>{sculpture.description}</p>
+  </>
+
+  function incrementIndex() {
+    if (index >= sculptureList.length - 1) {
+      setIndex(0);
+      return;
+    }
+    setIndex(index + 1);
+  }
+
+  function setColor(newColor) {
+    console.log(newColor);
+    setColorState(newColor);
+  }
+
+  function getColor() {
+    console.log(colorState);
+  }
 }
 
 export default State;
